88da343593fa3c9b9775739029339f5bed714710
Asia Baj <asiabaj_7@o2.pl>
2022-11-26 12:47:31 +0100

    add project model

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
@@ -5,6 +7,18 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db = SQLAlchemy(app)
+
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    link = db.Column(db.String(100), nullable=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    finished = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return f'<Project {self.title}>'
 
 
 @app.route("/")
